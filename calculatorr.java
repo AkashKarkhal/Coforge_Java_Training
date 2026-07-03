29d88ec91bf5797e7f56d911598e671492a1a512
AkashKarkhal <150418371+AkashKarkhal@users.noreply.github.com>
2026-07-03 15:31:04 +0530

    Add files via upload
    Method Overloading With Calculator example...

---
+++ calculatorr.java
@@ -0,0 +1,32 @@
+public class calculatorr {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int add(int a, int b, int c) {
+        return a + b + c;
+    }
+
+    public int add(int a, int b, int c, int d) {
+        return a + b + c + d;
+    }
+
+    public double add(double a, double b) {
+        return a + b;
+    }
+
+    public double add(double a, double b, double c) {
+        return a + b + c;
+    }
+
+    public double add(double a, double b, double c, double d) {
+        return a + b + c + d;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
+
+
